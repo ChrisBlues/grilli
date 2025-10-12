bec6072249c6fdc0f857db96d6b2a0b2f4872630
ChrisBlues <chris.blues@free.fr>
2025-10-12 09:44:11 +0200

    Grilli avance bien (2:11:32 sur 4:23:05, plus de la moitié...) : mon écran montre un graphisme exceptionnel, c'est très beau !

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,9 @@
 
 
 /**
- * * PRELOAD
+ * PRELOAD
  * 
- * loading will be and after document is loaded
- * le chargement aura lieu et après le chargement du document
+ * loading will be end after document is loaded
  */
 
 const preloader = document.querySelector("[data-preaload]");
@@ -144,4 +143,27 @@ window.addEventListener("load", autoSlide);
 
 
 
- 
+/**
+ * PARALLAX EFFECT
+ */
+
+const parallaxItems = document.querySelectorAll("[data-parallax-item]");
+
+let x, y;
+
+window.addEventListener("mousemove", function (event) {
+
+    x = (event.clientX / window.innerWidth * 10) - 5;
+    y = (event.clientY / window.innerHeight * 10) - 5;
+
+    // reverse the number eg. 20 -> -20, -5 -> 5
+    x = x - (x * 2);
+    y = y - (y * 2);
+
+    for (let i = 0, len = parallaxItems.length; i < len; i++) {
+        x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+        y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+        parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+  }
+
+    });
