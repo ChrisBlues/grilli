2812d30bdaf12190a0f5bc60bf4573f60e235249
ChrisBlues <chris.blues@free.fr>
2025-10-14 19:07:01 +0200

    Affinage du footer : apparition juste en bas 🌒

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,9 +169,21 @@ window.addEventListener("mousemove", function (event) {
 });
 
 // Détection du bas de page pour les remerciements au concepteur
+// window.addEventListener('scroll', () => {
+//   const scrollPosition = window.scrollY + window.innerHeight;
+//   const pageHeight = document.body.offsetHeight;
+//   const footer = document.getElementById('thank-you-footer');
+
+//   if (scrollPosition >= pageHeight - 50) {
+//     footer.classList.add('visible');
+//   } else {
+//     footer.classList.remove('visible');
+//   }
+// });
+
 window.addEventListener('scroll', () => {
   const scrollPosition = window.scrollY + window.innerHeight;
-  const pageHeight = document.body.offsetHeight;
+  const pageHeight = document.documentElement.scrollHeight;
   const footer = document.getElementById('thank-you-footer');
 
   if (scrollPosition >= pageHeight - 50) {
